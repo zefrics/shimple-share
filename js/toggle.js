bdf4abe945efc8e233a8f1af6e778ff09bd7af47
zefrics <zefrics@naver.com>
2023-07-09 16:53:10 +0900

    20230709
    1. 게시판 시스템 수정중

--- js/toggle.js
+++ js/toggle.js
@@ -11,12 +11,17 @@ function detail(e) { //게시판 접고 펴는 기능
   }
 }
 
+function detail2(e) { // 모바일 게시판 접고 펴는 기능
+  e.parentNode.classList.toggle("selected");
+}
+
+
 function filter(e) { //필터 접고 펴는 기능
   var child1 = e.parentNode.querySelector(".options"); //클릭한 요소의 세대 중 options를 캐치
   child1.classList.toggle("hide");
 }
 
-function filter2(e) { //필터 접고 펴는 기능
+function filter2(e) { // 모바일 필터 접고 펴는 기능
   var child2 = e.querySelector(".options"); //클릭한 요소의 자손 중 options를 캐치
   child2.classList.toggle("hide");
 }
